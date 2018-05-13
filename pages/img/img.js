c6ac3d75d388bbc27e466fa85d672dc1ffba912f
zhangliucheng <414616504@qq.com>
2018-05-13 23:34:52 +0800

    develop

--- pages/img/img.js
+++ pages/img/img.js
@@ -8,24 +8,31 @@ function Picture(parent, imgPath, shapeRect) {
   this.shapeRect = shapeRect;
   this.offsetX = 0;
   this.offsetY = 0;
-  this.scale = 1;
+  this.scale = 1.2;
   this.float = false;
   this.canMove = false;
   this.canScale = false;
   this.canChange = false;
 
+  this.reset = function () {
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.scale = 1.2;
+    this.float = false;
+    this.canMove = false;
+    this.canScale = false;
+    this.canChange = false;
+  }
+
   this.draw = function (ctx) {
-    // console.log(x, y, scaleWidth, scaleHeight)
-    // console.log(this.offsetX, this.offsetY, this.scale)
     if (this.float) {
       ctx.save()
       ctx.setGlobalAlpha(0.5)
       ctx.beginPath()
-      let floatW = this.shapeRect.width / 2;
-      let floatH = this.shapeRect.height / 2;
-      let x = this.shapeRect.x + this.offsetX + floatW / 2;
-      let y = this.shapeRect.y + this.offsetY + floatH / 2;
-      ctx.drawImage(this.imgPath, x, y, floatW, floatH);
+
+      let x = this.touchX - this.shapeRect.width / 3;
+      let y = this.touchY - this.shapeRect.height / 3;
+      ctx.drawImage(this.imgPath, x, y, this.shapeRect.width / 1.5, this.shapeRect.height / 1.5);
       ctx.restore();
     } else {
       ctx.save()
@@ -33,21 +40,19 @@ function Picture(parent, imgPath, shapeRect) {
       ctx.rect(this.shapeRect.x, this.shapeRect.y, this.shapeRect.width, this.shapeRect.height)
       ctx.closePath();
       ctx.clip();
-
-      // console.log(this.name, this.offsetY)
       let scaleWidth = this.shapeRect.width * this.scale;
       let scaleHeight = this.shapeRect.height * this.scale;
       let x = this.shapeRect.x + this.offsetX + (this.shapeRect.width - scaleWidth) / 2;
       let y = this.shapeRect.y + this.offsetY + (this.shapeRect.height - scaleHeight) / 2;
+      // console.log(x, y, scaleWidth, scaleHeight)
+      // console.log(this.offsetX, this.offsetY)
       ctx.drawImage(this.imgPath, x, y, scaleWidth, scaleHeight);
-
       if (this.canChange) {
         ctx.rect(this.shapeRect.x, this.shapeRect.y, this.shapeRect.width, this.shapeRect.height)
         ctx.setStrokeStyle('#3C8DEF')
         ctx.setLineWidth(5)
         ctx.stroke()
       }
-
       ctx.restore();
     }
   }
@@ -55,7 +60,7 @@ function Picture(parent, imgPath, shapeRect) {
   this.touchstart = function (e) {
     if (e.touches.length == 1) {
       if (this.shapeRect.contains(e.touches[0].x, e.touches[0].y)) {
-        this.canMove = true;
+        this.canMove = true; 
         this.parent.moveTarget = this;
         this.touchX = e.touches[0].x
         this.touchY = e.touches[0].y
@@ -69,8 +74,6 @@ function Picture(parent, imgPath, shapeRect) {
         let xMove = e.touches[1].x - e.touches[0].x;
         let yMove = e.touches[1].y - e.touches[0].y;
         let distance = Math.sqrt(xMove * xMove + yMove * yMove);
-        // console.log('双手指触发开始 ---', xMove, yMove)
-        // console.log('双手指触发开始 ===', distance, e)
         this.distance = distance
       }
     }
@@ -79,12 +82,14 @@ function Picture(parent, imgPath, shapeRect) {
   this.touchmove = function (e) {
     if (e.touches.length == 1) {
       if (this.canMove == true) {
-        let newX = (e.touches[0].x - this.touchX) + this.originX
-        let newY = (e.touches[0].y - this.touchY) + this.originY
-        this.offsetX = newX
-        this.offsetY = newY
+        let newX = (e.touches[0].x - this.touchX) + this.originX;
+        let newY = (e.touches[0].y - this.touchY) + this.originY;
+        this.offsetX = newX;
+        this.offsetY = newY;
         if (false == this.shapeRect.contains(e.touches[0].x, e.touches[0].y)) {
           this.float = true;
+          this.touchX = e.touches[0].x
+          this.touchY = e.touches[0].y
         }
       }
       if (this != this.parent.moveTarget && this.shapeRect.contains(e.touches[0].x, e.touches[0].y)) {
@@ -97,23 +102,41 @@ function Picture(parent, imgPath, shapeRect) {
       let yMove = e.touches[1].y - e.touches[0].y;
       let distance = Math.sqrt(xMove * xMove + yMove * yMove);
       let distanceDiff = distance - this.distance;
-      let newScale = this.scale + 0.005 * distanceDiff
-      if (newScale >= 3) {
-        newScale = 3
+      let newScale;
+      if (distanceDiff >= 0) {
+        newScale = this.scale + 0.008 * distanceDiff;
+      } else {
+        newScale = this.scale + 0.03 * distanceDiff;
+      }
+      if (newScale >= 6) {
+        newScale = 6;
       }
       if (newScale <= 1) {
-        newScale = 1
+        newScale = 1;
       }
-      this.distance = distance
-      this.scale = newScale
-      console.log(newScale)
+      this.distance = distance;
+      this.scale = newScale;
     }
   }
 
   this.touchend = function (e) {
-    if (this.canMove == true && this.parent.changeTarget != undefined) {
-      this.offsetX = 0;
-      this.offsetY = 0;
+    // if (this.canMove == true && this.parent.changeTarget != undefined) {
+    //   this.offsetX = 0;
+    //   this.offsetY = 0;
+    // }
+    let x = (this.shapeRect.width * this.scale - this.shapeRect.width) / 2;
+    let y = (this.shapeRect.height * this.scale - this.shapeRect.height) / 2;
+    if (this.offsetX > x) {
+        this.offsetX = x;
+    }
+    if (this.offsetY > y) {
+      this.offsetY = y;
+    }
+    if (this.offsetX < -x) {
+      this.offsetX = -x;
+    }
+    if (this.offsetY < -y) {
+      this.offsetY = -y;
     }
     this.float = false;
     this.canMove = false;
@@ -137,7 +160,7 @@ Page({
         wx.getImageInfo({
           src: imgPath,
           success: function (res) {
-            let rect = new shpae.rect(10, 10, 200, 200);
+            let rect = new shpae.rect(50, 60, 200, 200);
             that.pic = new Picture(that, imgPath, rect);
             if (that.p2 == true) {
               that.interval = setInterval(that.drawImg, 30)
@@ -194,21 +217,11 @@ Page({
   touchendCallback: function (e) {
     this.pic.touchend(e);
     this.pic1.touchend(e);
-    // console.log('=====touchendCallback=====', this.moveTarget.imgPath)
-
-    // this.changeTarget.imgPath = 'http://a4.topitme.com/o/201101/29/12962866459127.jpg';
-    // this.moveTarget.imgPath = 'http://a4.topitme.com/o/201101/29/12962866459127.jpg';
-    // console.log('=====touchendCallback=====', this.moveTarget.imgPath)
-
     if (this.moveTarget != undefined && this.changeTarget != undefined) {
       let tmp = this.moveTarget.imgPath;
-      // console.log('==========', this.moveTarget.imgPath, this.changeTarget.imgPath)
-
       this.moveTarget.imgPath = this.changeTarget.imgPath;
       this.changeTarget.imgPath = tmp;
-
-      // console.log('==========', this.moveTarget.imgPath, this.changeTarget.imgPath)
-
+      this.moveTarget.reset();
     }
     this.moveTarget = undefined;
     this.changeTarget = undefined;
